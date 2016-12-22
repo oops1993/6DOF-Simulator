420f219fd43da72014a70fc9332e9131a7748354
oops1993 <chenweibo93@gmail.com>
2016-12-22 13:00:26 +0800

    submit

--- main.py
+++ main.py
@@ -72,20 +72,6 @@ if I2C:
     changeServoAngle([45., 135., 45., 135., 45., 135.])
     changeCrankAngle([45., 135., 45., 135., 45., 135.])
 if False:
-    # Simple demo of of the PCA9685 PWM servo/LED controller library.
-    # This will move channel 0 from min to max position repeatedly.
-    # Author: Tony DiCola
-    # License: Public Domain
-    from __future__ import division
-    import time
-
-    # Import the PCA9685 module.
-    import Adafruit_PCA9685
-
-
-    # Uncomment to enable debug output.
-    #import logging
-    #logging.basicConfig(level=logging.DEBUG)
 
     # Initialise the PCA9685 using the default address (0x40).
     pwm = Adafruit_PCA9685.PCA9685()
