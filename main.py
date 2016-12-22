7a4ef7d2543ccf67a02e77dc52e81b8d8857c2e1
oops1993 <chenweibo93@gmail.com>
2016-12-22 12:51:34 +0800

    Submit

--- main.py
+++ main.py
@@ -7,42 +7,45 @@ import numpy.matlib as ml
 import time
 # Import the PCA9685 module.
 I2C = False
+
 try:
     import Adafruit_PCA9685
 except Exception as ex1:
     print("Failed to import Adafruit_PCA9685.\n")
+pwm = 0
+servoFrequency = 50
+servoMinDutyCycle = 2.5
+servoMaxDutyCycle = 12.5
+servoPinNumber = (0,1,2,3,4,5)
+crank2servo = (60.,-60.,60.,-60.,60.,-60.)
+servoInitialAngle = (90.,90.,90.,90.,90.,90.)
 
-if I2C:
-    servoFrequency = 50
-    servoMinDutyCycle = 2.5
-    servoMaxDutyCycle = 12.5
-    servoPinNumber = (0,1,2,3,4,5)
-    crank2servo = (60.,-60.,60.,-60.,60.,-60.)
-    servoInitialAngle = (90.,90.,90.,90.,90.,90.)
-
-    def servoInitialize():
-        global servoInitialAngle, pwm, servoFrequency
+def servoInitialize():
+    global servoInitialAngle, pwm, servoFrequency
+    if I2C:
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(servoFrequency)
         changeServoAngle(servoInitialAngle)
 
-    def changeServoAngle(angleList):
-        global servoMinDutyCycle, servoMinDutyCycle, servoPinNumber, pwm
-        def pl(angle):
-            if angle>=0. and angle<=180.:
-                return floor((angle/(servoMinDutyCycle-servoMinDutyCycle)+servoMinDutyCycle)/40.96+0.5)
-            elif angle<0.:
-                return floor((servoMinDutyCycle)/40.96+0.5)
-            elif angle>180.:
-                return floor((servoMaxDutyCycle)/40.96+0.5)
-        for i in (0,1,2,3,4,5):
+def changeServoAngle(angleList):
+    global servoMinDutyCycle, servoMinDutyCycle, servoPinNumber, pwm
+    def pl(angle):
+        if angle>=0. and angle<=180.:
+            return floor((angle/(servoMaxDutyCycle-servoMinDutyCycle)+servoMinDutyCycle)*40.96+0.5)
+        elif angle<0.:
+            return floor((servoMinDutyCycle)*40.96+0.5)
+        elif angle>180.:
+            return floor((servoMaxDutyCycle)*40.96+0.5)
+    for i in (0,1,2,3,4,5):
+        if I2C:
             pwm.set_pwm(servoPinNumber[i], 0 , pl(angleList[i]))
-    def changeCrankAngle(angleList):
-        global crank2servo
+def changeCrankAngle(angleList):
+    global crank2servo
+    if I2C:
         changeServoAngle(map(lambda (a,b):a+b, zip(angleList,crank2servo)))
-
+if I2C:
     servoInitialize()
-    time.sleep(1)
+time.sleep(1)
 
 a = sixDof.sixDofSimPltfm()
 Rot1 = sixDof.calcTransformMatrix(\
@@ -59,4 +62,5 @@ a.calcBearingAngle()
 print a.checkBearingAngle(),'\n'
 a.pltfmPosition.printAngles()
 if I2C:
+    changeServoAngle([45., 135., 45., 135., 45., 135.])
     changeCrankAngle([45., 135., 45., 135., 45., 135.])
